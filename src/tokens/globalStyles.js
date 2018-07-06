77fe386f98444b6045d323f440c40ab9756273e3
Norbert Tóth <norbert@quince.hu>
2018-07-07 00:18:33 +0200

    Design tokens, theming and globalsytle edits

--- src/tokens/globalStyles.js
+++ src/tokens/globalStyles.js
@@ -1,38 +1,56 @@
 import { injectGlobal } from 'styled-components';
 import styledNormalize from 'styled-normalize';
-import { colors, fonts } from '.';
+import tokens from '.';
 
 // Global styles for the whole application
 // using 'injectGlobal' from styled-components
 // see: https://www.styled-components.com/docs/api#injectglobal
-
-const GlobalStyles = () => injectGlobal`
+const globalStyles = () => injectGlobal`
     ${styledNormalize}
 
     body,
     html {
-        background-color: ${colors.white};
-        color: ${fonts.colors.default};
-        font-family: ${fonts.sets.body.default};
+        background-color: ${tokens.colors.white};
+        color: ${tokens.colors.black};
+        font-family: ${tokens.fonts.sets.body.default};
         font-feature-settings: 'kern', 'liga', 'clig', 'calt';
         font-kerning: normal;
-        font-size: ${fonts.body.default};
+        font-size: ${tokens.fonts.sizes.body.default};
         font-style: normal;
         font-variant-ligatures: common-ligatures contextual;
-        font-weight: ${fonts.weights.default};
+        font-weight: ${tokens.fonts.weights.default};
         min-height: 100vh;
         min-width: 100vw;
         text-rendering: optimizeLegibility;
+
+    &:not(.user-is-tabbing) {
+        button:focus,
+        input:focus,
+        select:focus,
+        textarea:focus {
+            outline: none;
+        }
+    }
+
+    fieldset {
+        border: 0;
+        padding: 0;
+        margin: 0;
     }
 
     *,
     *::before,
     *::after {
         box-sizing: border-box;
-        -webkit-margin-before: 0;
+
         -webkit-margin-after: 0;
+        -webkit-margin-before: 0;
         -webkit-padding-start: 0;
+        margin-block-end: 0;
+        margin-block-start: 0;
+        margin-inline-end: 0;
+        margin-inline-start: 0;
     }
 `;
 
-export default GlobalStyles;
+export default globalStyles;
--- src/tokens/index.js
+++ src/tokens/index.js
@@ -1,3 +1,53 @@
-export { default as colors } from './_tokens/colors';
-export { default as GlobalStyles } from './globalStyles';
-export { default as fonts } from './_tokens/fonts';
+import { rem, em } from 'polished';
+
+// unitless(but means pixel values) base sizes to use in calculations
+const appFontSize = 16;
+const baseSpacing = 16;
+
+const tokens = {
+    colors: {
+        black: 'hsla(0, 0%, 0%, 1)',
+        white: 'hsla(0, 0%, 100%, 1)',
+        gray: {
+            light: 'hsla(0, 0%, 76%, 1)',
+            dark: 'hsla(0, 0%, 40%, 1)',
+        },
+        primary: 'hsla(216, 71%, 66%, 1)',
+        secondary: 'hsla(211, 100%, 95%, 1)',
+    },
+    fonts: {
+        weights: {
+            default: 'normal',
+            bold: 'bold',
+        },
+        sizes: {
+            body: {
+                default: rem('16px', appFontSize),
+                small: rem('12px', appFontSize),
+            },
+        },
+        sets: {
+            body: {
+                // Default font set (with system fonts)
+                default:
+                    '-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+            },
+        },
+    },
+    // Breakpoints
+    breakpoints: ['40em', '52em', '64em'],
+    // @media screen and (min-width: 40em)
+    // @media screen and (min-width: 52em)
+    // @media screen and (min-width: 64em)
+
+    // Spacing Scale (used for margin and padding)
+    space: {
+        tiny: em(0.25 * baseSpacing, appFontSize),
+        small: em(0.5 * baseSpacing, appFontSize),
+        normal: em(1 * baseSpacing, appFontSize),
+        large: em(2 * baseSpacing, appFontSize),
+        huge: em(4 * baseSpacing, appFontSize),
+    },
+};
+
+export default tokens;
